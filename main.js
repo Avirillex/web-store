eb5b963e208feccb4906a0883bb7b8140e8c8fef
Avirillex <jonathansvm01@gmail.com>
2023-03-21 17:06:04 -0500

    shopping cart merged

--- main.js
+++ main.js
@@ -2,15 +2,40 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuBurguer = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.container');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector(".product-detail");
+
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 menuBurguer.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    console.log("Click")
-    desktopMenu.classList.toggle("inactive")
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+
+    desktopMenu.classList.toggle("inactive");
 }
 
 function toggleMobileMenu() {
-    mobileMenu.classList.toggle("inactive")
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive");
+    }
+    
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+
+    if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("inactive");
+    }
+    /* menuDesktop: not yet */
+    aside.classList.toggle("inactive");
 }
