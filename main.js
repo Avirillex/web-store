5978bf6b8f067f3aaf6ab85423b49938fa0f6431
Avirillex <jonathansvm01@gmail.com>
2023-03-20 19:55:23 -0500

    merge mobile menu

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.container');
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuBurguer.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     console.log("Click")
     desktopMenu.classList.toggle("inactive")
 }
 
-console.log("JS funcionando")
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive")
+}
